07d01ae41f7d7d837339f74ce6c1cbdea666dc97
Tomas Castaño <150631481+AsaultCT@users.noreply.github.com>
2026-08-18 02:10:57 -0500

    Clean up

--- Proveedor.py
+++ Proveedor.py
@@ -1,7 +1,3 @@
-#Este archivo fue trabajado por todos los integrantes del grupo en conjunto por medio de la extension de Visual Studio Code "Live Share"
-# Juan Pablo Arruba
-# Tomas Castaño Taborda
-# Israel Cañizalez Mongua
 
 from colorama import Fore
 import numpy as np
